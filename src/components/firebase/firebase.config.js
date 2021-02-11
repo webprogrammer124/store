3e9384d32944e32a496b9954e4e98b2a767b8fd6
HifsaAnsari <hifsa.ansari12@gmail.com>
2021-02-11 18:42:18 +0500

    ordernow

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC-zWiZcpM-cdTFj96cGCRKz2I_5plpvsE",
+    authDomain: "ecofirebaseproject-d2735.firebaseapp.com",
+    projectId: "ecofirebaseproject-d2735",
+    storageBucket: "ecofirebaseproject-d2735.appspot.com",
+    messagingSenderId: "427248203585",
+    appId: "1:427248203585:web:1b36bf0f89fbb21eee93c0",
+    measurementId: "G-ZVV4HY7XC1"
+  };
+
+
+export default firebaseConfig;
