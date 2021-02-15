99d6ba8bcfe8f9a6367f15c082e1a5f208940875
seema <seemagreat.911@gmail.com>
2021-02-15 08:12:57 +0500

    firebase

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -1,12 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC-zWiZcpM-cdTFj96cGCRKz2I_5plpvsE",
-    authDomain: "ecofirebaseproject-d2735.firebaseapp.com",
-    projectId: "ecofirebaseproject-d2735",
-    storageBucket: "ecofirebaseproject-d2735.appspot.com",
-    messagingSenderId: "427248203585",
-    appId: "1:427248203585:web:1b36bf0f89fbb21eee93c0",
-    measurementId: "G-ZVV4HY7XC1"
+  apiKey: "AIzaSyCS_Lgkez_MGiZaYOF_S8oKBpNZhBfhcbc",
+  authDomain: "ecommerce-ec590.firebaseapp.com",
+  projectId: "ecommerce-ec590",
+  storageBucket: "ecommerce-ec590.appspot.com",
+  messagingSenderId: "363291596646",
+  appId: "1:363291596646:web:f584340c36156229b49aa7"
   };
 
 
